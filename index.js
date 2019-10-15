0897682d94ba7991c0f4b1071bfb2dd1632839e1
Stephen <jinyaoye23@126.com>
2019-10-16 07:05:04 +0800

    iOS 上传设置图标代码

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import { NativeModules } from 'react-native';
 
-const { CustomIcon } = NativeModules;
+const CustomIconModule  = NativeModules.CustomIcon;
 
-export default CustomIcon;
+export default class CustomIcon {
+  // 更新图标
+  static changeIcon(iconName) {
+    CustomIconModule.changeIcon(iconName);
+  }
+  // 恢复默认图标
+  static changeDefaultIcon() {
+    CustomIconModule.changeDefaultIcon();
+  }
+};
--- ios/CustomIcon.m
+++ ios/CustomIcon.m
@@ -1,14 +1,53 @@
 #import "CustomIcon.h"
+#import <UIKit/UIKit.h>
 
 
 @implementation CustomIcon
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-	callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+
+RCT_EXPORT_METHOD(changeIcon:(NSString *)iconName) {
+    UIApplication *app = [UIApplication sharedApplication];
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        if (![[UIApplication sharedApplication] supportsAlternateIcons]) {
+            return;
+        }
+        
+    });
+    
+    if ([iconName isEqualToString:@""]) {
+        iconName = nil;
+    }
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
+            if (error) {
+                NSLog(@"更换app图标发生错误了 ： %@",error);
+            }
+        }];
+    });
 }
+
+// 恢复默认图标
+RCT_EXPORT_METHOD(changeDefaultIcon) {
+    UIApplication *app = [UIApplication sharedApplication];
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        if (![[UIApplication sharedApplication] supportsAlternateIcons]) {
+            return;
+        }
+        
+    });
+    
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        [[UIApplication sharedApplication] setAlternateIconName:nil completionHandler:^(NSError * _Nullable error) {
+            if (error) {
+                NSLog(@"更换app图标发生错误了 ： %@",error);
+            }
+        }];
+    });
+}
+
+
+
 
 @end

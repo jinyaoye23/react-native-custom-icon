96ef3338dc76cc0a7913596f2b5c586076d5b263
shizhenzhen <kumbaya123@126.com>
2019-10-16 14:54:19 +0800

    android端添加更换图标的逻辑代码

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { NativeModules } from 'react-native';
 const CustomIconModule  = NativeModules.CustomIcon;
 
 export default class CustomIcon {
+  static registerIcon(iconNameList) {
+    CustomIconModule.registerIcon(iconNameList);
+  }
   // 更新图标
   static changeIcon(iconName) {
     CustomIconModule.changeIcon(iconName);

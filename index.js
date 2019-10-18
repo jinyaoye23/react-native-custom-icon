58d816468eb737d2054012c1852664f9b0f0f5f8
shizhenzhen <kumbaya123@126.com>
2019-10-18 11:10:05 +0800

    统一android和ios双端的方法和参数

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
-const CustomIconModule  = NativeModules.CustomIcon;
-
+const CustomIconModule = NativeModules.CustomIcon;
+const isIOS = Platform.OS === 'ios'
 export default class CustomIcon {
   static registerIcon(iconNameList) {
-    CustomIconModule.registerIcon(iconNameList);
+    console.log('isIOS = ',isIOS)
+    console.log('iconNameList = ',iconNameList)
+
+    if (!isIOS) {
+      CustomIconModule.registerIcon(iconNameList);
+    }
   }
   // 更新图标
-  static changeIcon(iconName) {
+  static changeIcon(id) {
+    let iconName = isIOS ? id + '-icon' : id
     CustomIconModule.changeIcon(iconName);
   }
   // 恢复默认图标
   static changeDefaultIcon() {
-    CustomIconModule.changeDefaultIcon();
+    if (isIOS) {
+      CustomIconModule.changeDefaultIcon();
+    } else {
+      CustomIconModule.changeIcon('');
+    }
   }
 };
